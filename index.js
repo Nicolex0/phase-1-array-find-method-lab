27f386203ba572457d33a6a2d4264f4b5227ed4f
Nicole Apono <napononicole@gmail.com>
2024-03-26 23:39:37 +0300

    index.js

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// code your solution here
+const superbowls = [
+    { year: 2020, result: "L" },
+    { year: 2021, result: "W" },
+    { year: 2022, result: "L" },
+    { year: 2023, result: "W" }
+];
+
+function superbowlWin(superbowls) {
+    const win = superbowls.find(superbowl => superbowl.result === "W");
+    return win ? win.year : undefined;
+}
